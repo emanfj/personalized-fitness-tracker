3e7fe0e720385237e4836c9303fc43e64a5eff4c
emanfj <syedaeman999@gmail.com>
2025-06-18 14:23:09 +0500

    removed prev files

--- view-col.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-from delta import configure_spark_with_delta_pip
-
-spark = configure_spark_with_delta_pip(SparkSession.builder).getOrCreate()
-spark.read.format('delta').load('lake/bronze/fitness_events').show(5)
-spark.read.format('delta').load('lake/silver/fitness_aggregates').show(5)
